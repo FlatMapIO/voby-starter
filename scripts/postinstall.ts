51fb476d775ea5ffc18898486e24db9eaf013f07
dong.huo <huodon@gmail.com>
2023-07-22 02:08:58 +0800

    fix postinstall script

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -1,15 +1,12 @@
 import { existsSync, rmSync } from 'node:fs'
+import { join as joinPath } from 'node:path'
+import { cwd } from 'node:process'
 
 function removeVobyTsConfig() {
-  const files = [
-    'node_module/voby/tsconfig.json',
-    'node_module/oby/tsconfig.json',
-  ]
-  files.forEach((file) => {
-    if (existsSync(file)) {
-      rmSync(file)
-    }
-  })
+  const p = (m: string) => joinPath(cwd(), 'node_modules', m, 'tsconfig.json')
+  const rm = (m: string) => existsSync(p(m)) && rmSync(p(m))
+  rm('voby')
+  rm('oby')
 }
 
 removeVobyTsConfig()
